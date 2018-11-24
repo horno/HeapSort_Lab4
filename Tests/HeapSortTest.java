baf0e90ea38ad1dd3f4515e277185140bd3db079
horno <i.palacin.a@gmail.com>
2018-11-24 15:24:18 +0100

    Tests package and class added

---
+++ Tests/HeapSortTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HeapSortTest {
+
+    @Test
+    public void sort() {
+    }
+
+    @Test
+    public void sort1() {
+    }
+}
