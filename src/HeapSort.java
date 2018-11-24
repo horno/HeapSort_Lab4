4eaf98d84515c81660fd4a7640077545bc1c72cc
horno <i.palacin.a@gmail.com>
2018-11-24 11:43:50 +0100

    Commit

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -1,5 +1,16 @@
-public class HeapSort {
-    public HeapSort(){
+import java.util.ArrayList;
+import java.util.Comparator;
 
+public class HeapSort{
+
+    private static class Heap<E>{
+        private  ArrayList<E> elements;
+        private Comparator<? super E> comparator;
+
+        private int heapSize = 0;
+
+        public Heap(){
+
+        }
     }
 }
